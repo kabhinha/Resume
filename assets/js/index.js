6afd663013e1374d8ed9ec01421afebed9b80fad
Abhishek <abhishekmalhotra1292@gmail.com>
2023-02-02 19:29:22 +0530

    Bug Fixed

---
+++ assets/js/index.js
@@ -0,0 +1,31 @@
+function hovers() {
+    document.getElementById('skills').innerHTML = "<h1>Skills</h1><p>Leadership, Critical Thinking<br><br>Python, C, C++, Julia, GoLang<br><br>HTML 5, CSS 3, Bootstrap 4<br></p>";
+}
+
+function outs() {
+    document.getElementById('skills').innerHTML = "<h2>Skills</h2>";
+}
+
+function hovered() {
+        document.getElementById('edu').innerHTML = "<h2>Education</h2><p><b>2021-2022 <em>Greenfields Public School, Delhi (CBSE)</em></b><br>Secured 91.4% in 10+2</p> <br><p><b>2019-2020 <em>Greenfields Public School, Delhi (CBSE)</em></b><br>Secured 89.4% in 10</p> <br><p><b>2022-NOW <em>IMS Engineering College, Ghaziyabad (AKTU)</em></b><br>Pursing B.Tech in CSE</p>;";
+}
+
+function outed() {
+    document.getElementById("edu").innerHTML = "<h1>Education</h1>";
+}
+
+function hoverax() {
+        document.getElementById('ax').innerHTML = "<h2>Achievements</h2><p><b>2022 <em>Patents</em></b><br>Published a patent research paper in the field of bio science<br><b>2022 <em>Hacker rank</em></b><br>3 star python coder and 2 star c coder at hackerank<br></p>";
+    }
+
+function outax() {
+    document.getElementById('ax').innerHTML = "<h1>Achievements</h1>";
+}
+
+function hoverpr() {
+        document.getElementById('pro').innerHTML = "<h2>Projects</h2><p><b>December 2022 <em>Preety_Good_Privacy</em></b><br>It is a customized hashmap to ensure PGP. <br><b>August 2022 <em>PDF Merger</em></b><br>With the help of this program, user can merge different <br>pdfs located at different paths(folders) into a single pdf. <br><b>July 2022 <em>Chup Chup Expenditure </em></b><br>This program can be called a virtual balance sheet with the <br>help of which user can track the record of <br>Transaction made in cash by entering the data <br></p>";
+    }
+
+function outpr() {
+    document.getElementById('pro').innerHTML = "<h1>Projects</h1>";
+}
